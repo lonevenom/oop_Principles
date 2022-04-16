37a1e12c1837694a1ce41506c6c1bc7dc15c7c4f
lonevenom <mz3ghett0@gmail.com>
2022-04-16 15:41:24 -0500

    added OverridingMethod

--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -1,6 +1,7 @@
 package inheritance;
 
 public class Tester extends Person{
+
     public Tester(){
 
     }
@@ -10,8 +11,26 @@ public class Tester extends Person{
     }
 
     public void code(){
-        System.out.println("Tester code");
+        System.out.println("Tester codes");
     }
 
+    @Override
+    public void eat(){
+        System.out.println("Tester eats");
+    }
 
+    @Override
+    public void sleep() {
+        System.out.println("Tester sleeps");
+    }
+
+    @Override
+    public void learn() {
+        System.out.println("Tester learns");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("Tester walks");
+    }
 }
